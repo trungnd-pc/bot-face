12f0b943f10ed1c6572500872788c852e3b0c12e
trungnd <trungnd.pc@gmail.com>
2017-04-08 11:59:58 +0700

    second commit

--- server.js
+++ server.js
@@ -72,9 +72,8 @@ function sendMessage(senderId, message) {
 
 
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
+app.set('port', process.env.PORT || 3002);
 
-server.listen(app.get('port'), app.get('ip'), function() {
+server.listen(app.get('port'), function() {
   console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
 });
