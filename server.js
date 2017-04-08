a8c7767407b24160e6038229e6cd31cc1fc8d3c4
trungnd <trungnd.pc@gmail.com>
2017-04-08 15:23:17 +0700

    test menu persistent

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ app.post('/webhook', function(req, res) {
 
 // Gửi thông tin tới REST API để trả lời
 function sendMessage(senderId, message) {
+  addPersistentMenu();
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
     qs: {
@@ -70,6 +71,56 @@ function sendMessage(senderId, message) {
   });
 }
 
+
+function addPersistentMenu() {
+  request({
+      url:'https://graph.facebook.com/v2.6/me/messenger_profile',
+      qs: {
+          access_token: "EAACNJHU49IwBAJbZCuyuRfX3iXTKsnFtYaBkoFv9qUx5dyNYklsK0ZCRwkZAl3t6nqmr67efzQANSwbCOjZBQvsrxXxp4uGTCjtVHW86zN7bQH0tq947AF5AfBYkcwOjswOnFdgxsavRLYOR9St7WafykLbKeXngPeYBX9ARkwZDZD",
+      },
+      method: 'POST',
+      persistent_menu:[
+          {
+              "locale":"default",
+              "composer_input_disabled":true,
+              "call_to_actions":[
+                  {
+                      "title":"My Account",
+                      "type":"nested",
+                      "call_to_actions":[
+                          {
+                              "title":"Pay Bill",
+                              "type":"postback",
+                              "payload":"PAYBILL_PAYLOAD"
+                          },
+                          {
+                              "title":"History",
+                              "type":"postback",
+                              "payload":"HISTORY_PAYLOAD"
+                          },
+                          {
+                              "title":"Contact Info",
+                              "type":"postback",
+                              "payload":"CONTACT_INFO_PAYLOAD"
+                          }
+                      ]
+                  },
+                  {
+                      "type":"web_url",
+                      "title":"Latest News",
+                      "url":"http://petershats.parseapp.com/hat-news",
+                      "webview_height_ratio":"full"
+                  }
+              ]
+          },
+          {
+              "locale":"zh_CN",
+              "composer_input_disabled":false
+          }
+      ]
+  })
+
+}
 
 
 app.set('port', process.env.PORT || 3002);
